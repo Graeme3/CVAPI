33dc007850bf19b26e0d145da66bc0e77c6ded40
Graeme Hancock <Graeme.Hancock@nologostudios.com>
2018-08-27 16:45:48 +0200

    Update the college

--- Entities/Models/College.cs
+++ Entities/Models/College.cs
@@ -11,9 +11,9 @@ namespace Entities.Models
         public string CollageName { get; set; }
         public string DiplomaName { get; set; }
         public string FileName { get; set; }
-        public Dictionary<string,int> FirstYearCourse { get; set; }
-        public Dictionary<string, int> SecondYearCourse { get; set; }
-        public Dictionary<string, int> ThirdYearCourse { get; set; }
+        public Dictionary<string,string> FirstYearCourse { get; set; }
+        public Dictionary<string, string> SecondYearCourse { get; set; }
+        public Dictionary<string, string> ThirdYearCourse { get; set; }
         public Guid SimpleFileGuid { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateOfUpdate { get; set; }
